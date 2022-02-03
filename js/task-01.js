7f86dcf18205fd16950d53c8a2da88d12f78a790
AnnaKulib <83002978+AnnaKulib@users.noreply.github.com>
2022-02-03 17:01:38 +0200

    task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,43 @@
-const cathegoryElements = document.querySelectorAll('.item');
+// const cathegoriesList = document.querySelector('.categories');
+const cathegoriesItems = document.querySelectorAll('.item');
 
-// console.log(cathegoryElements);
-console.log(`Number of categories: ${cathegoryElements.length}`);
+console.log(`Number of categories: ${cathegoriesItems.length}`);
 
-const cathegoryHeader = document.querySelectorAll('h2');
-
-// console.log(cathegoryHeader);
-
-cathegoryHeader.forEach(element => {
-    // console.log(element);
-    console.log(`Category: ${element.textContent}`);
-    console.log(`Elements: ${element.nextElementSibling.children.length}`);
+cathegoriesItems.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}` );
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
+
+
+
+// console.log(cathegoriesList);
+// console.log(cathegoriesItems);
+
+// cathegoriesItems.forEach(item => {
+    // console.log(item.parentNode);
+    // console.log(item.childNodes);
+    // console.log(item.children);
+    // console.log(item.firstChild);
+    // console.log(item.firstElementChild);
+    // console.log(item.firstElementChild.textContent);
+    // console.log(item.lastChild);
+    // console.log(item.lastElementChild);
+    // console.log(item.lastElementChild.children);
+    // console.log(item.lastElementChild.children.length);
+    // console.log(item.previousSibling);
+    // console.log(item.previousElementSibling);
+    // console.log(item.nextSibling);
+    // console.log(item.nextElementSibling);
+// })
+
+// elem.parentNode - выберет родителя elem.
+// elem.childNodes - псевдомассив, хранит все дочерние элементы, включая текстовые.
+// elem.children - псевдомассив, хранит только дочерние узлы-элементы, то есть соответствующие тегам.
+// elem.firstChild - выберет первый дочерний элемент внутри elem, включая текстовые узлы.
+// elem.firstElementChild - выберет первый дочерний узел-элемент внутри elem.
+// elem.lastChild - выберет последний дочерний элемент внутри elem, включая текстовые узлы.
+// elem.lastElementChild - выберет последний дочерний узел-элемент внутри elem.
+// elem.previousSibling - выберет элемент «слева» от elem (его предыдущего соседа).
+// elem.previousElementSibling - выберет узел-элемент «слева» от elem (его предыдущего соседа).
+// elem.nextSibling - выберет элемент «справа» от elem (его следующего соседа)
+// elem.nextElementSibling - выберет узел-элемент «справа» от elem (его следующего соседа).
