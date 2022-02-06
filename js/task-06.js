7bd1bd2e7c0365860b11e77b7f2d0fd2ffae1bcc
AnnaKulib <83002978+AnnaKulib@users.noreply.github.com>
2022-02-06 21:44:41 +0200

    task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#validation-input');
+const validLength = Number(inputEl.dataset.length);
+
+inputEl.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+    const inputValueLength = event.currentTarget.value.length;
+    inputEl.classList.add (inputValueLength !== validLength ? 'invalid' : 'valid');
+}
